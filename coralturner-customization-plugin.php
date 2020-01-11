b40851cd7bda5d1daa5872f4ecd1d8c99d6fe5d7
Alex <alex@moise.pro>
2020-01-11 18:52:31 +0200

    Move title above thumb and wrap the thumb with its link

--- coralturner-customization-plugin.php
+++ coralturner-customization-plugin.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://github.com/alexmoise/coralturner-customization-plugin
  * GitHub Plugin URI: https://github.com/alexmoise/coralturner-customization-plugin
  * Description: A custom plugin to add required customizations to Coral Turner Woocommerce shop and to style the front end as required. Works based on Woocommerce and Clothing69 theme. For details/troubleshooting please contact me at https://moise.pro/contact/
- * Version: 0.0.5
+ * Version: 0.0.6
  * Author: Alex Moise
  * Author URI: https://moise.pro
  */
@@ -21,28 +21,30 @@ function moctcp_adding_styles() {
 // Moving some WooCommerce stuff around so the products looks like magazine covers :-)
 add_action('init', 'moctcp_layout_adjustments');
 function moctcp_layout_adjustments() {
-	// remove price and buy button in product lists:
+	// Remove price and buy button in product lists:
 	remove_action('woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart');
 	remove_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_price', 10 );
-
-	// Remove the title from its original place
+	// Remove the title from its original place:
 	remove_action('woocommerce_before_shop_loop_item_title', 'clothing69_woocommerce_item_wrapper_start', 9 );
 	remove_action('woocommerce_before_shop_loop_item_title', 'clothing69_woocommerce_title_wrapper_start', 20 );
 	remove_action('woocommerce_shop_loop_item_title', 'woocommerce_template_loop_product_title', 10);
 	remove_action('woocommerce_after_shop_loop_item_title', 'clothing69_woocommerce_title_wrapper_end', 7);
 	remove_action('woocommerce_after_shop_loop_item', 'clothing69_woocommerce_item_wrapper_end', 20 );
-	// Then add it back above the thumbnail
-	add_action('woocommerce_before_shop_loop_item', 'clothing69_woocommerce_item_wrapper_start', 100 );
+	// Then add it back above the thumbnail:
+	add_action('woocommerce_before_shop_loop_item', 'clothing69_woocommerce_item_wrapper_start', 90 );
 	add_action('woocommerce_before_shop_loop_item', 'clothing69_woocommerce_title_wrapper_start', 110 );
 	add_action('woocommerce_before_shop_loop_item', 'woocommerce_template_loop_product_title', 120);
 	add_action('woocommerce_before_shop_loop_item', 'clothing69_woocommerce_title_wrapper_end', 130);
 	add_action('woocommerce_before_shop_loop_item', 'clothing69_woocommerce_item_wrapper_end', 140 );
+	// Wrap thumbnail with link:
+	add_action('woocommerce_before_shop_loop_item_title', function(){echo '<a href='; echo get_the_permalink(); echo '>';}, 9);
+	add_action('woocommerce_before_shop_loop_item_title', function(){echo '</a>';}, 11);
 }
 
 // change jpeg quality a bit:
 add_filter('jpeg_quality', function($arg){return 90;});
 
-// change number of products
+// change number of products per page
 add_filter( 'loop_shop_per_page', 'new_loop_shop_per_page', 20 );
 function new_loop_shop_per_page( $cols ) {
   $cols = 9;
