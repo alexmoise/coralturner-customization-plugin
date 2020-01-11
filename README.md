8b68e97c2e5757db42f9824472302e44cb83111b
Alex <alex@moise.pro>
2020-01-11 13:52:00 +0200

    Initial PHP file added

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# coralturner-customization-plugin
+# Coralturner Customization Plugin
 A custom plugin to add required customizations to Coral Turner Woocommerce shop and to style the front end as required. Works based on Woocommerce and Clothing69 theme. For details/troubleshooting please contact me at https://moise.pro/contact/
---
+++ coralturner-customization-plugin.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: Coralturner.com customizations plugin
+ * Plugin URI: https://github.com/alexmoise/coralturner-customization-plugin
+ * GitHub Plugin URI: https://github.com/alexmoise/coralturner-customization-plugin
+ * Description: A custom plugin to add required customizations to Coral Turner Woocommerce shop and to style the front end as required. Works based on Woocommerce and Clothing69 theme. For details/troubleshooting please contact me at https://moise.pro/contact/
+ * Version: 0.0.1
+ * Author: Alex Moise
+ * Author URI: https://moise.pro
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {	exit(0);}
+
+
+
