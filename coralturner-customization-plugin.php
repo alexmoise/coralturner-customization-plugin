5224060212113cc485921d4f54d5f7c5f35a8179
Alex <alex@moise.pro>
2020-01-27 18:28:53 +0200

    Fixing "magazine" height of related products

--- coralturner-customization-plugin.php
+++ coralturner-customization-plugin.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://github.com/alexmoise/coralturner-customization-plugin
  * GitHub Plugin URI: https://github.com/alexmoise/coralturner-customization-plugin
  * Description: A custom plugin to add required customizations to Coral Turner Woocommerce shop and to style the front end as required. Works based on Woocommerce and Clothing69 theme. For details/troubleshooting please contact me at https://moise.pro/contact/
- * Version: 0.0.7
+ * Version: 0.0.8
  * Author: Alex Moise
  * Author URI: https://moise.pro
  */
